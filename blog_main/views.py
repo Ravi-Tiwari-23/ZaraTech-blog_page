75973670c029069c0086c3ae7e8027dd3c573bf4
Ravi Kumar Tiwari <ravi.tiwari2303@gmail.com>
2026-08-04 23:58:46 +0530

    Update blog homepage and fix static assets

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,5 +1,15 @@
 
 from django.shortcuts import render
+from blogs.models import Category , Blog
 
 def home(request):
-    return render(request, "home.html")
+    categories = Category.objects.all()
+    featured_post = Blog.objects.filter(is_featured = True).order_by('updated_at')
+    
+
+    context = {
+        'categories': categories,
+        'featured_post' : featured_post,
+    }
+
+    return render(request, "home.html", context)
