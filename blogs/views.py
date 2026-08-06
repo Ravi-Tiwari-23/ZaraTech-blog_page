d21df8720dca60044de47691cf622d9f796df253
Ravi Kumar Tiwari <ravi.tiwari2303@gmail.com>
2026-08-06 23:46:01 +0530

    Update navbar

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
+
+
+def posts_by_category(request, category_id):
+    return HttpResponse('Posts by category')
 
 # Create your views here.
