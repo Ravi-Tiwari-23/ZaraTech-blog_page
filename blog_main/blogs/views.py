3cea05a673ffb1368c8318e267ff87283e71512d
Ravi Kumar Tiwari <ravi.tiwari2303@gmail.com>
2026-08-12 23:48:40 +0530

    blogs and search features

--- blog_main/blogs/views.py
+++ blog_main/blogs/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render ,get_object_or_404, redirect
 
 from blogs.models import Blog , Category
+from django.db.models import Q
 
 
 
@@ -28,5 +29,23 @@ def posts_by_category(request, category_id):
         'category_name': category.category_name,
     }
     return render(request, 'posts_by_category.html', context)
+
+def blogs(request, slug):
+    single_blog = get_object_or_404(Blog, slug = slug, status = 'Published')
+    context = {
+        'single_blog': single_blog,
+    }
+    return render(request , 'blogs.html' , context)
+
+def search(request):
+    keyword = request.GET.get('keyword')
+
+    blogs = Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains=keyword) | Q(blog_body__icontains=keyword), status = 'Published')
+    
+    context = {
+        'blogs': blogs,
+        'keyword': keyword,
+    }
+    return render(request, 'search.html', context)
 
 # Create your views here.
