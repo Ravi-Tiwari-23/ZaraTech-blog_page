d7acaa937ab44bf29065ee4802134132ddc22b04
Ravi Kumar Tiwari <ravi.tiwari2303@gmail.com>
2026-08-19 02:35:38 +0530

    created category delete edit functionality

--- blog_main/blog_main/urls.py
+++ blog_main/blog_main/urls.py
@@ -19,7 +19,6 @@ from django.urls import path , include
 from . import views
 from django.conf.urls.static import static 
 from django.conf import settings
-from django.conf import settings
 from blogs import views as BlogsView
 urlpatterns = [
     path('admin/', admin.site.urls),
--- blog_main/blog_main/views.py
+++ blog_main/blog_main/views.py
@@ -50,8 +50,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user is not None:
                 auth.login(request,user)
-
-            return redirect ('dashbord')
+                return redirect ('/dashboard/')
     form = AuthenticationForm()
     context = {
         'form': form,
